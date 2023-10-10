f89cd536b449bff07ba9ac85658680c3ff529f0f
panventi <3341708470@qq.com>
2023-10-10 20:47:16 +0800

    实验四修改

--- app/src/main/java/com/jnu/student/MainActivity.java
+++ app/src/main/java/com/jnu/student/MainActivity.java
@@ -1,24 +1,58 @@
 package com.jnu.student;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-//import android.widget.LinearLayout;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView1, textView2;
+
+    @SuppressLint({"SetTextI18n", "MissingInflatedId"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_4);
 
-        // 找到TextView控件
-        TextView textViewHelloWorld = findViewById(R.id.text_view_hello_world);
+        textView1 = findViewById(R.id.hello);
+        textView2 = findViewById(R.id.jnu);
+        Button button = findViewById(R.id.button);
+
+        textView1.setText("Hello");
+        textView2.setText("JNU");
+
+        button.setOnClickListener(v -> {
+            String temp = textView1.getText().toString();
+            textView1.setText(textView2.getText().toString());
+            textView2.setText(temp);
+            showToast();
+            showAlertDialog();
+        });
+    }
+
+    private void showToast() {
+        Toast.makeText(this, "交换成功", Toast.LENGTH_SHORT).show();
+    }
+
+    private void showAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("交换成功")
+                .show();
+    }
+
+
+
+
+    // 找到TextView控件
+        //TextView textViewHelloWorld = findViewById(R.id.text_view_hello_world);
 
         // 获取字符串资源并设置到TextView中
-        String helloAndroid = getResources().getString(R.string.hello_android);
-        textViewHelloWorld.setText(helloAndroid);
+        //String helloAndroid = getResources().getString(R.string.hello_android);
+        //textViewHelloWorld.setText(helloAndroid);
 
 
         /*
@@ -32,5 +66,5 @@ public class MainActivity extends AppCompatActivity {
 
          setContentView(layout);
         */
-    }
+
 }
